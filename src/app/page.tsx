42a2a466415daa922e05fd4095de53f444b1b3fd
Felipe E. Schmidt <schmidtfe2@gmail.com>
2025-05-08 19:34:04 -0300

    feat: creating category handle to get it from the searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,46 @@
 "use client";
 
-import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 
-import { CardListing } from "@/components/CardListing";
 import { useCatsGetter } from "@/hooks/useCatsGetter";
+import { CardListing } from "@/components/CardListing";
+import { CardListingLoading } from "@/components/CardListing/loading";
 
 import styles from "./page.module.css";
 
-export default function CatGallery({ categoryId }: { categoryId: number }) {
+export default function CatGallery() {
+  const searchParams = useSearchParams();
+  const categoryIdParam = searchParams.get("categoryId");
+
+  const categoryId = categoryIdParam ? Number(categoryIdParam) : undefined;
+
   const { data, error, isLoading, isLoadingMore, loadNextPage, setFilters } =
-    useCatsGetter();
+    useCatsGetter(categoryId);
 
-  useEffect(() => {
-    setFilters((prev) => ({ ...prev, categoryId }));
-  }, [categoryId, setFilters]);
-
-  return (
-    <main className={styles.page}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Cat Encyclopedia</h1>
+  const renderPrincipal = () => {
+    if (isLoading && !data.length) {
+      return <CardListingLoading forceOnlyOneLine />;
+    } else if (error) {
+      return <div>{error}</div>;
+    }
+    return (
+      <>
         <CardListing
           data={data}
           imageField="url"
           titleField="name"
           countyCodeField="countyCode"
         />
+        {isLoadingMore && <CardListingLoading forceOnlyOneLine />}
+      </>
+    );
+  };
+
+  return (
+    <main className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Cat Encyclopedia</h1>
+        {renderPrincipal()}
       </div>
     </main>
   );
--- src/hooks/useCatsGetter.ts
+++ src/hooks/useCatsGetter.ts
@@ -5,12 +5,12 @@ import { useCallback, useEffect, useState } from "react";
 import { CatsFiltersAvailable, fetchCatsPaginated } from "@/lib/api/cats";
 import { CatDetailedNormalized } from "@/types";
 
-export const useCatsGetter = () => {
+export const useCatsGetter = (categoryId?: number) => {
   const [page, setPage] = useState(1);
   const [error, setError] = useState<string>();
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<CatDetailedNormalized[]>([]);
-  const [filters, setFilters] = useState<CatsFiltersAvailable>({});
+  const [filters, setFilters] = useState<CatsFiltersAvailable>({ categoryId });
 
   const loadNextPage = useCallback(() => {
     setPage((prev) => prev + 1);
